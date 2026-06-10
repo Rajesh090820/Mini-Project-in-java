69ae3d929f13d1d9f033a2aa6be48cbcd00bc7c8
Rajesh090820 <rajeshsn2000@gmail.com>
2026-06-10 09:31:59 +0530

    Delete main1.java

--- main1.java
+++
@@ -1,8 +0,0 @@
-public class main1 {
-    public void fullThrottle(){
-        System.out.println("The car is going on the maximum throttle");
-    }
-    public void maxspeed(int speed){
-        System.out.println("Max speed is : " + speed);
-    }
-}
